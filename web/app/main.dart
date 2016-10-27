04744d38148452cf45e26919930ce9a35bcdff69
just nobody <kingdaro@gmail.com>
2016-10-27 17:52:34 -0400

    restructure event loop

--- web/app/main.dart
+++ web/app/main.dart
@@ -5,13 +5,15 @@ import 'game.dart';
 main() async {
   final CanvasElement canvas = querySelector('#game');
   final game = new Game(canvas);
-  var time = await window.animationFrame;
+  num time = new DateTime.now().millisecondsSinceEpoch;
 
-  while (true) {
-    final now = await window.animationFrame;
+  frame(now) {
     final elapsed = now - time;
     game.update(elapsed / 1000);
     game.draw();
     time = now;
+    window.requestAnimationFrame(frame);
   }
+
+  window.requestAnimationFrame(frame);
 }
