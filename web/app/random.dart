628309addc7b5f2dfc43ce5fa9dcb8b1ee38ec7e
just nobody <kingdaro@gmail.com>
2016-10-28 02:44:09 -0400

    annotate arguments

--- web/app/random.dart
+++ web/app/random.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 final random = new Random();
 
 /// Returns an integer between min and max (exclusive)
-int intRange(min, max) => random.nextInt(max) + min;
+int intRange(int min, int max) =>
+  random.nextInt(max) + min;
 
 /// Returns a double between min and max
-double doubleRange(min, max) => random.nextDouble() * (max - min) + min;
+double doubleRange(double min, double max) =>
+  random.nextDouble() * (max - min) + min;
