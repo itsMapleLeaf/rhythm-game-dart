355973760ca1316c36edf37f2076440f27e58caa
just nobody <kingdaro@gmail.com>
2016-10-29 17:26:33 -0400

    documented the clock

--- web/app/clock.dart
+++ web/app/clock.dart
@@ -1,16 +1,29 @@
+/// For working with recurring tasks.
+///
+/// This is coded so the clock doesn't fall behind when the time goes too
+/// far beyond the limit.
+///
+/// Example:
+/// Clock clock = new Clock(0.5); // a clock that ticks every 0.5 seconds
+/// clock.update(1); // will trigger two ticks.
 class Clock {
+  /// The current clock's time
   num time = 0;
+
+  /// The time limit for each tick
   final num limit;
 
   Clock(this.limit);
 
-  int update(num dt, [onTick]) {
+  /// Update the clock's time. Returns the number of ticks performed.
+  /// Optionally, pass a function that's called once for every tick.
+  int update(num dt, [onTick()]) {
     time += dt;
-    num ticks = 0;
-    while (time > limit) {
+    int ticks = 0;
+    while (time >= limit) {
       time -= limit;
       ticks += 1;
-      onTick();
+      if (onTick != null) onTick();
     }
     return ticks;
   }
