0fa6942a38978b8b82bcfd6ebad439dc48a08119
just nobody <kingdaro@gmail.com>
2016-10-30 20:47:15 -0400

    more compact formatting

--- web/app/game.dart
+++ web/app/game.dart
@@ -6,23 +6,10 @@ import 'game_state.dart';
 class Game {
   GameState state;
 
-  Game() {
-    state = new Gameplay();
-  }
+  Game() : state = new Gameplay();
 
-  update(num dt) {
-    state = state.update(dt);
-  }
-
-  draw() {
-    state.draw();
-  }
-
-  keydown(KeyboardEvent event) {
-    state.keydown(event);
-  }
-
-  keyup(KeyboardEvent event) {
-    state.keyup(event);
-  }
+  update(num dt) => state = state.update(dt);
+  draw() => state.draw();
+  keydown(KeyboardEvent event) => state.keydown(event);
+  keyup(KeyboardEvent event) => state.keyup(event);
 }
