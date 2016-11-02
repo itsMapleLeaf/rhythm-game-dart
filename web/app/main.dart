724f48e11792248eaed1ca9073190395bcd5a7b6
just nobody <kingdaro@gmail.com>
2016-11-02 17:24:22 -0400

    rename some notefield methods

--- web/app/main.dart
+++ web/app/main.dart
@@ -2,32 +2,29 @@ import 'dart:html' show querySelector, CanvasElement, window;
 
 import 'color.dart' show Color;
 import 'notefield.dart' show drawNotefield;
-import 'graphics.dart' show clear;
+import 'graphics.dart' show clear, reset, layer;
 
 /// Model representing the game's state
 class GameModel {}
 
 main() async {
   final CanvasElement canvas = querySelector('#game');
-  final ctx = canvas.context2D;
 
   while (true) {
     await window.animationFrame;
 
-    ctx.resetTransform();
+    reset(canvas);
     clear(canvas, Color.white);
-
-    ctx..save()..translate(250, 0);
-
-    drawNotefield(canvas, 48, 6, [
-      Color.yellow,
-      Color.white,
-      Color.violet,
-      Color.white,
-      Color.violet,
-      Color.white,
-    ]);
-
-    ctx.restore();
+    layer(canvas, () {
+      canvas.context2D.translate(250, 0);
+      drawNotefield(canvas, 48, 6, [
+        Color.yellow,
+        Color.white,
+        Color.violet,
+        Color.white,
+        Color.violet,
+        Color.white,
+      ]);
+    });
   }
 }
--- web/app/notefield.dart
+++ web/app/notefield.dart
@@ -1,7 +1,7 @@
 import 'dart:html' show CanvasElement;
 
 import 'color.dart' show Color;
-import 'graphics.dart' show drawRectangle;
+import 'graphics.dart' show layer, drawRectangle;
 
 /// Draws the notefield
 drawNotefield(
@@ -13,31 +13,31 @@ drawNotefield(
   final ctx = canvas.context2D;
   final totalWidth = columnWidth * columnCount;
   final coverColor = Color.black;
-  drawNotefieldCover(canvas, totalWidth, coverColor);
+  drawCover(canvas, totalWidth, coverColor);
 
-  ctx.save();
-  for (int i = 0; i < columnCount; i++) {
-    drawNotefieldBacklight(canvas, columnWidth, columnColors[i]);
-    drawNotefieldKey(canvas, columnWidth, columnColors[i]);
-    drawNotefieldReceptor(canvas, columnWidth, columnColors[i]);
-    ctx.translate(columnWidth, 0);
-  }
-  ctx.restore();
+  layer(canvas, () {
+    for (int i = 0; i < columnCount; i++) {
+      drawBacklight(canvas, columnWidth, columnColors[i]);
+      drawKey(canvas, columnWidth, columnColors[i]);
+      drawReceptor(canvas, columnWidth, columnColors[i]);
+      ctx.translate(columnWidth, 0);
+    }
+  });
 }
 
-drawNotefieldCover(CanvasElement canvas, num width, Color color) {
+drawCover(CanvasElement canvas, num width, Color color) {
   drawRectangle(canvas, 0, 0, width, canvas.height, color.withOpacity(0.8));
 }
 
-drawNotefieldBacklight(CanvasElement canvas, num width, Color color) {
+drawBacklight(CanvasElement canvas, num width, Color color) {
   drawRectangle(canvas, 0, 0, width, canvas.height, color.withOpacity(0.08));
 }
 
-drawNotefieldKey(CanvasElement canvas, num width, Color color) {
+drawKey(CanvasElement canvas, num width, Color color) {
   drawRectangle(canvas, 0, canvas.height, width, -100, color);
 }
 
-drawNotefieldReceptor(CanvasElement canvas, num width, Color color) {
+drawReceptor(CanvasElement canvas, num width, Color color) {
   color = color.withOpacity(0.3);
   drawRectangle(canvas, 0, canvas.height - 100, width, -24, color);
 }
