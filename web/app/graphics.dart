e45e2e2b016ee9045921d79dd25a2e3a2f01e71b
just nobody <kingdaro@gmail.com>
2016-11-02 17:24:06 -0400

    add a few convenience functions to graphics

--- web/app/graphics.dart
+++ web/app/graphics.dart
@@ -9,8 +9,21 @@ drawRectangle(CanvasElement canvas, num x, num y, num width, num height, Color c
     ..fillRect(x, y, width, height);
 }
 
+/// Clear the canvas area to a given color
 clear(CanvasElement canvas, Color color) {
   canvas.context2D
     ..fillStyle = color
     ..fillRect(0, 0, canvas.width, canvas.height);
+}
+
+/// Reset the canvas transform state
+reset(CanvasElement canvas) {
+  canvas.context2D.resetTransform();
+}
+
+/// Perform drawing transforms within a transformation context
+layer(CanvasElement canvas, drawOps()) {
+  canvas.context2D.save();
+  drawOps();
+  canvas.context2D.restore();
 }
