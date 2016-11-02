7ef78fd2fe9ba86149df2500078c7e843831ca81
just nobody <kingdaro@gmail.com>
2016-11-02 18:17:09 -0400

    pretty triangles.

--- web/app/bg_animation.dart
+++ web/app/bg_animation.dart
@@ -4,30 +4,48 @@ import 'graphics.dart';
 import 'util.dart';
 
 class BGShape {
-  num x, y;
+  num x, y, size;
+  Color color;
+
+  draw() {
+    drawPolygon(x, y, size, 3, color);
+  }
 }
 
 class BGAnimation {
+  static final shapeColors = [
+    Color.red,
+    Color.orange,
+    Color.yellow,
+    Color.green,
+    Color.blue,
+    Color.violet,
+  ];
+
   final shapes = [];
-  final shapeClock = new Clock(0.3);
+  final shapeClock = new Clock(0.15);
 
   update(num dt) {
     shapeClock.update(dt, addShape);
     shapes
-      ..forEach((shape) => shape.y -= 100 * dt)
-      ..retainWhere((shape) => shape.y > -100);
+      ..forEach((shape) => shape.y -= shape.size * dt)
+      ..retainWhere((shape) => shape.y > -shape.size);
   }
 
   addShape() {
-    shapes.add(new BGShape()
+    final size = random(80, 160);
+    final shape = new BGShape()
       ..x = random(0, canvas.width)
-      ..y = canvas.height + 100
-    );
+      ..y = canvas.height + size
+      ..size = size
+      ..color = shapeColors[random(0, shapeColors.length).floor()];
+
+    shapes
+      ..add(shape)
+      ..sort((a, b) => a.size - b.size);
   }
 
   draw() {
-    for (final shape in shapes) {
-      drawPolygon(shape.x, shape.y, 100, 3, Color.blue);
-    }
+    shapes.forEach((shape) => shape.draw());
   }
 }
