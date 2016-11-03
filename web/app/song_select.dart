6a57269136ab1b6eb31f5c4fcd00e7b9e97e00f8
just nobody <kingdaro@gmail.com>
2016-11-03 18:01:04 -0400

    songwheel animation

--- web/app/song_select.dart
+++ web/app/song_select.dart
@@ -2,6 +2,7 @@ import 'dart:html' show KeyboardEvent, KeyCode;
 
 import 'game.dart';
 import 'graphics.dart';
+import 'util.dart';
 
 class SongSelect implements GameState {
   final songs = [
@@ -15,22 +16,27 @@ class SongSelect implements GameState {
   ];
 
   int currentSong = 0;
+  num visualOffset = 0;
 
   keydown(KeyboardEvent event) {
     if (event.keyCode == KeyCode.UP) {
       currentSong -= 1;
+      visualOffset = -100;
     } else if (event.keyCode == KeyCode.DOWN) {
       currentSong += 1;
+      visualOffset = 100;
     }
     currentSong = currentSong % songs.length;
   }
 
   keyup(KeyboardEvent event) {}
 
-  update(num dt) {}
+  update(num dt) {
+    visualOffset = lerp(visualOffset, 0, dt * 20);
+  }
 
   draw() {
-    num pos = canvas.height / 2 - 126 * currentSong;
+    num pos = canvas.height / 2 - 126 * currentSong + visualOffset;
     for (final song in songs) {
       drawText(song['title'], 20, pos, Color.asphalt, Font.roboto(72));
       drawText(song['artist'], 20, pos + 50, Color.asphalt, Font.roboto(54));
