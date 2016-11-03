c5906c6c83a721420981bba240e0cd671557e082
just nobody <kingdaro@gmail.com>
2016-11-03 18:11:15 -0400

    fleshed out song select animation

--- web/app/song_select.dart
+++ web/app/song_select.dart
@@ -1,4 +1,5 @@
 import 'dart:html' show KeyboardEvent, KeyCode;
+import 'dart:math' show PI;
 
 import 'game.dart';
 import 'graphics.dart';
@@ -36,11 +37,19 @@ class SongSelect implements GameState {
   }
 
   draw() {
-    num pos = canvas.height / 2 - 126 * currentSong + visualOffset;
-    for (final song in songs) {
-      drawText(song['title'], 20, pos, Color.asphalt, Font.roboto(72));
-      drawText(song['artist'], 20, pos + 50, Color.asphalt, Font.roboto(54));
-      pos += 126;
-    }
+    layer(() {
+      canvas.context2D
+        ..translate(0, canvas.height / 2)
+        ..translate(0, -126 * currentSong)
+        ..translate(0, visualOffset);
+
+      num pos = 0;
+      for (final song in songs) {
+        drawText(song['title'], canvas.width / 2, pos, Color.asphalt, Font.roboto(72));
+        drawText(song['artist'], canvas.width / 2, pos + 50, Color.asphalt, Font.roboto(54));
+        pos += 126;
+      }
+      drawPolygon(600, 126 * currentSong, 20, 3, Color.asphalt);
+    });
   }
 }
