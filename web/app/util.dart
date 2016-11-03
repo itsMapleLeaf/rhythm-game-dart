dc582d5bbb35b740e57595b9b791940159839560
just nobody <kingdaro@gmail.com>
2016-11-03 16:48:10 -0400

    clamp the delta between 0 and 1

--- web/app/util.dart
+++ web/app/util.dart
@@ -8,7 +8,7 @@ num lerp(num a, num b, num delta) => a + (b - a) * delta.clamp(0, 1);
 
 /// Find the delta of a value between a and b
 /// ex: delta(2, 1, 3) == 0.5
-num delta(num value, num a, num b) => a != b ? (value - a) / (b - a) : 0;
+num delta(num value, num a, num b) => a != b ? ((value - a) / (b - a)).clamp(0, 1) : 0;
 
 /// Return a random number between a and b
 num random(num a, num b) => _rgen.nextDouble() * (b - a) + a;
